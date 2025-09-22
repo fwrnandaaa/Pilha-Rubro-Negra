77f7836f6b1b359c51e363e6b1e7e3a27d7777d4
Fernanda Dantas <110307073+fwrnandaaa@users.noreply.github.com>
2025-09-22 23:46:36 +0000

    Adicionando testes do top vermelho e preto

--- testePilha.java
+++ testePilha.java
@@ -10,11 +10,18 @@ public class testePilha {
 		  System.out.println(f);		  
 		  pp.push_vermelho(new Integer(f));
 		}
+		try {
+            System.out.println("Topo vermelho: " + pp.top_vermelho());
+        } 
+		catch (PilhaVaziaExcecao e) {
+            System.out.println(e.getMessage());
+        }
 		System.out.println("retirando - Pilha vermelha");
 		for(int f=0;f<10;f++){
 			  System.out.print(f);
 			  System.out.println(" - "+pp.pop_vermelho());
 		}
+	
 
 		//pilha preta
 		System.out.println("inserindo - Pilha preta");
@@ -22,6 +29,11 @@ public class testePilha {
 		  System.out.println(f);		  
 		  pp.push_preto(new Integer(f));
 		}
+		try {
+                System.out.println("Topo preto: " + pp.top_preto());
+            } catch (PilhaVaziaExcecao e) {
+                System.out.println(e.getMessage());
+            }
 		System.out.println("retirando - Pilha preta");
 		for(int f=0;f<10;f++){
 			  System.out.print(f);
