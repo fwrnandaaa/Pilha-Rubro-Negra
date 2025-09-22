23114eff66cc6e2bb450f2204a1d05a84d0ae559
Fernanda Dantas <110307073+fwrnandaaa@users.noreply.github.com>
2025-09-22 17:31:28 +0000

    Implementações da pilha vermelha

--- PilhaArray.java
+++ PilhaArray.java
@@ -14,9 +14,10 @@ public class PilhaArray implements Pilha {
             crescimento = 0;
         }
     }
+    // Vou usar o override para os metodos da pilha vermelha. Os métodos da pilha preta serão implementados sem override.
     @Override
     public int size(){
-        return t+1;
+        return topoV+1;
     }
 
     @Override
@@ -31,16 +32,35 @@ public class PilhaArray implements Pilha {
         if(isEmpty()){
             throw new PilhaVaziaExcecao("A pilha está vazia"); 
         }
-        return a[t];
+        return a[topoV];
     }
     @Override
     public void push(Object o){
+        aumenta_pilha_vermelha();
+        a[++topoV] = o;
+
 
     }
     @Override
     public Object pop() throws PilhaVaziaExcecao{
-        Object r=a[t--];
+         if(isEmpty()){
+            throw new PilhaVaziaExcecao("A pilha está vazia"); 
+        }
+        Object r=a[topoV--];
         return r;
+    }
+    public void aumenta_pilha(){
+        if (topoV+1 == capacidade){
+            capacidade = capacidade*2;
+            Object[] novo_array = new Object[capacidade];
+            for(int i = 0; i< a.length; i++){
+                novo_array[i] = a[i];
+            }
+            a = novo_array;
+        }
+    }
+    public void reducao_pilha(){
+        if((topoV+1 + capacidade - topoP) <= capacidade)
     }
 }
 
